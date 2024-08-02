e6c6f9d9903ad57ea5f4ec19b527054a82369cec
Elena Kiseljova <elena.ki@webest.ru>
2024-08-02 21:37:37 +0300

    add: default chart Bar

---
+++ pages/index.vue
@@ -0,0 +1,50 @@
+<script setup lang="ts">
+import {
+  Chart as ChartJS,
+  Title,
+  Tooltip,
+  Legend,
+  BarElement,
+  CategoryScale,
+  LinearScale,
+} from 'chart.js';
+import { Bar } from 'vue-chartjs';
+
+// Register components of Chart
+ChartJS.register(
+  Title,
+  Tooltip,
+  Legend,
+  BarElement,
+  CategoryScale,
+  LinearScale
+);
+</script>
+
+<template>
+  <div class="charts">
+    <Bar
+      :data="{
+        labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+        datasets: [
+          {
+            label: '# of Votes',
+            data: [12, 19, 3, 5, 2, 3],
+            borderWidth: 1,
+          },
+        ],
+      }"
+      :options="{
+        scales: {
+          y: {
+            beginAtZero: true,
+          },
+        },
+      }"
+      class="charts__bar"
+    />
+  </div>
+</template>
+
+<style scoped></style>
+
